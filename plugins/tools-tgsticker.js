54eb67c9182a84b871641f37fb94427c9d775280
Mr unique Hacker <149955557+techsamie789@users.noreply.github.com>
2023-11-06 01:24:18 +0530

    Update tools-tgsticker.js

--- plugins/tools-tgsticker.js
+++ plugins/tools-tgsticker.js
@@ -5,8 +5,8 @@ var handler = async (m, { conn, args }) => {
 	if (args[0] && args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) {
 		let res = await Telesticker(args[0])
 		await m.reply(`Sending ${res.length} stickers...`)
-		if (m.isGroup && res.length > 30) {
-			await m.reply('Number of stickers more than 30, bot will send it in private chat.')
+		if (m.isGroup && res.length > 130) {
+			await m.reply('Number of stickers more than 130, bot will send it in private chat.')
 			for (let i = 0; i < res.length; i++) {
 				conn.sendMessage(m.sender, { sticker: { url: res[i].url }})
 			}
